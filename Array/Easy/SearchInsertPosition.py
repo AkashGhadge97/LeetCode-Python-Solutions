28a23e57b1d68086bdc6bc688e0837d7c492791b
Akash Ghadge <30373328+AkashGhadge97@users.noreply.github.com>
2023-07-08 22:37:59 +0530

    Update SearchInsertPosition.py

--- Array/Easy/SearchInsertPosition.py
+++ Array/Easy/SearchInsertPosition.py
@@ -1,9 +1,14 @@
-#Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+#Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order. T.C = O(logn)
 class Solution(object):
    def searchInsert(self, nums, target):
-        if target in nums:
-            return nums.index(target)
-        else:
-            nums.append(target)
-            nums=sorted(nums)
-            return nums.index(target)
+        lower = 0
+        upper = len(nums)
+        while (lower < upper):
+            mid = int((lower+upper)/2)
+            if target == nums[mid]:
+                return mid
+            elif target > nums[mid]:
+                lower = mid + 1
+            else:
+                upper = mid
+        return lower
