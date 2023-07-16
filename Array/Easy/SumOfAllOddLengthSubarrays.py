f148f89f71503f01c757cd8cdb9a77424d1b7d57
Akash Ghadge <30373328+AkashGhadge97@users.noreply.github.com>
2023-07-17 00:30:35 +0530

    Create SumOfAllOddLengthSubarrays.py

---
+++ Array/Easy/SumOfAllOddLengthSubarrays.py
@@ -0,0 +1,17 @@
+# Given an array of positive integers arr, return the sum of all possible odd-length subarrays of arr.
+
+# A subarray is a contiguous subsequence of the array.
+
+class Solution(object):
+    def sumOddLengthSubarrays(self, arr):
+        total = 0
+        for i in range(0, len(arr)):
+            for j in range(1, len(arr)+1):
+                print(arr[i:j])
+                if (len(arr[i:j]) % 2 == 1):
+                    total += sum(arr[i:j])
+        return total
+
+
+result = Solution().sumOddLengthSubarrays([1, 4, 2, 5, 3])
+print(result)
