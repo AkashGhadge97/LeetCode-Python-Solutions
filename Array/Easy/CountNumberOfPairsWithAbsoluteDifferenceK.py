4e78c88384803c0bfe5daadf087552689deded0a
Akash Ghadge <30373328+AkashGhadge97@users.noreply.github.com>
2023-07-19 22:36:10 +0530

    Create CountNumberOfPairsWithAbsoluteDifferenceK.py

---
+++ Array/Easy/CountNumberOfPairsWithAbsoluteDifferenceK.py
@@ -0,0 +1,14 @@
+# Given an integer array nums and an integer k, return the number of pairs (i, j) where i < j such that |nums[i] - nums[j]| == k.
+
+# The value of |x| is defined as:
+
+# x if x >= 0.
+# -x if x < 0.
+
+class Solution(object):
+    def countKDifference(self, nums, k):
+        return sum(nums.count(num+k) for num in nums if num+k in nums)
+
+
+result = Solution().countKDifference([1, 2, 2, 1], 1)
+print(result)
