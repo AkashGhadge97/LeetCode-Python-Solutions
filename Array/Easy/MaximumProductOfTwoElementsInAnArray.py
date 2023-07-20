03301c6a7ed062f2495d8708283c291f143c5d06
Akash Ghadge <30373328+AkashGhadge97@users.noreply.github.com>
2023-07-20 23:32:19 +0530

    Create MaximumProductOfTwoElementsInAnArray.py

---
+++ Array/Easy/MaximumProductOfTwoElementsInAnArray.py
@@ -0,0 +1,10 @@
+# Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
+
+class Solution(object):
+    def maxProduct(self, nums):
+        nums.sort()
+        return (nums[-1] - 1) * (nums[-2] - 1)
+
+
+result = Solution().maxProduct([1, 5, 4, 5])
+print(result)
