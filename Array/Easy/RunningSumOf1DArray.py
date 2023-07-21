6287c889062e2c9ee76da7624156a224a736052d
Akash Ghadge <30373328+AkashGhadge97@users.noreply.github.com>
2023-07-22 00:29:43 +0530

    Create RunningSumOf1DArray.py

---
+++ Array/Easy/RunningSumOf1DArray.py
@@ -0,0 +1,17 @@
+# Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+# Return the running sum of nums.
+
+
+class Solution(object):
+    def runningSum(self, nums):
+        final = []
+        total = 0
+        for i in nums:
+            total += i
+            final.append(total)
+        return final
+
+
+result = Solution().runningSum([1, 2, 3, 4])
+print(result)
