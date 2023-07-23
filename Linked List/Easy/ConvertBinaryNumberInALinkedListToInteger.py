de21e6173e7caf6530c78589e9bc20a79ad30976
Akash Ghadge <30373328+AkashGhadge97@users.noreply.github.com>
2023-07-23 21:24:42 +0530

    Create  ConvertBinaryNumberInALinkedListToInteger.py

---
+++ Linked List/Easy/ConvertBinaryNumberInALinkedListToInteger.py
@@ -0,0 +1,25 @@
+# Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+# Return the decimal value of the number in the linked list.
+# The most significant bit is at the head of the linked list.
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def getDecimalValue(self, head):
+        length=0
+        current = head
+        while current is not None:
+            current=current.next
+            length+=1
+        num = 0
+        current = head
+        power = length-1
+        while current is not None:
+            num+= ((2**(power))*current.val)
+            power-=1
+            current = current.next
+        return num
