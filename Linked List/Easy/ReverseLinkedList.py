0280f69a0f15d5bade2ffae18c99946635808b06
Akash Ghadge <30373328+AkashGhadge97@users.noreply.github.com>
2023-07-26 23:39:25 +0530

    Update ReverseLinkedList.py

--- Linked List/Easy/ReverseLinkedList.py
+++ Linked List/Easy/ReverseLinkedList.py
@@ -1,5 +1,4 @@
 #Given the head of a singly linked list, reverse the list, and return the reversed list.
-
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, val=0, next=None):
