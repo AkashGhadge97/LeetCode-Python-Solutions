8bba675e3ccadee7dff639ba79249325694e10fc
Akash Ghadge <30373328+AkashGhadge97@users.noreply.github.com>
2023-07-30 23:50:40 +0530

    Update MaximumProductDifferenceBetweenTwoPairs.py

--- Array/Easy/MaximumProductDifferenceBetweenTwoPairs.py
+++ Array/Easy/MaximumProductDifferenceBetweenTwoPairs.py
@@ -2,7 +2,6 @@
 
 # For example, the product difference between (5, 6) and (2, 7) is (5 * 6) - (2 * 7) = 16.
 # Given an integer array nums, choose four distinct indices w, x, y, and z such that the product difference between pairs (nums[w], nums[x]) and (nums[y], nums[z]) is maximized.
-
 # Return the maximum such product difference.
 
 class Solution(object):
