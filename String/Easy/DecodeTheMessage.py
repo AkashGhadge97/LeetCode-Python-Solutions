fd0fe0a45c39cf033396277568987f0865a36e4f
Akash Ghadge <30373328+AkashGhadge97@users.noreply.github.com>
2023-08-01 23:57:41 +0530

    Update DecodeTheMessage.py

--- String/Easy/DecodeTheMessage.py
+++ String/Easy/DecodeTheMessage.py
@@ -1,5 +1,4 @@
 # You are given the strings key and message, which represent a cipher key and a secret message, respectively. The steps to decode message are as follows:
-
 # Use the first appearance of all 26 lowercase English letters in key as the order of the substitution table.
 # Align the substitution table with the regular English alphabet.
 # Each letter in message is then substituted using the table.
