0d65a00904ff49db4fd46eb1b0a0c01d9c0a1bed
Akash Ghadge <30373328+AkashGhadge97@users.noreply.github.com>
2023-08-08 23:54:00 +0530

    Create CountingWordsWithAGivenPrefix.py

---
+++ String/Easy/CountingWordsWithAGivenPrefix.py
@@ -0,0 +1,12 @@
+# You are given an array of strings words and a string pref.
+# Return the number of strings in words that contain pref as a prefix.
+# A prefix of a string s is any leading contiguous substring of s.
+
+class Solution(object):
+    def prefixCount(self, words, pref):
+        lengthOfPref = len(pref)
+        count=0
+        for i in words:
+            if (i[0:lengthOfPref] == pref):
+                count+=1
+        return count
